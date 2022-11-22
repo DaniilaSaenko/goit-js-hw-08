befc4cbbe1e16fecca1ad7d101ad8fe4fed9e1f1
DaniilaSaenko <daniila.s777@gmail.com>
2022-11-22 06:01:03 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,38 @@
-import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const savedOnPlayPosition = localStorage.getItem(STORAGE_KEY);
+const parsedOnPlayPosition = JSON.parse(savedOnPlayPosition) || {};
+
+const onPlay = evt => {
+  const positionPlayer = {
+    duration: evt.duration,
+    percent: evt.percent,
+    seconds: evt.seconds,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(positionPlayer));
+};
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(parsedOnPlayPosition.seconds)
+  .then(function (seconds) {
+    seconds = parsedOnPlayPosition.seconds;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        'the time is less than 0 or greater than the videos duration';
+        break;
+      default:
+        'other error';
+        break;
+    }
+  });
