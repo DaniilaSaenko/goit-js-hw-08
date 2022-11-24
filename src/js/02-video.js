0e08f65d899b62351bc576ddae895451af761a68
DaniilaSaenko <daniila.s777@gmail.com>
2022-11-24 14:52:11 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// инициализация плейера
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-// ключ хранилища
+
 const STORAGE_KEY = 'videoplayer-current-time';
 
-//запись текущего времени в хранилище
-const onPlay = evt => {
-  const positionPlayer = evt.seconds;
+const onPlay = e => {
+  const positionPlayer = e.seconds;
   localStorage.setItem(STORAGE_KEY, positionPlayer);
 };
 
-// отслеживание текущего момента (событие timeupdate)
 player.on('timeupdate', throttle(onPlay, 1000));
 
-// возврат в хранилище сохраненного момента 
 const getCurrentTime = localStorage.getItem(STORAGE_KEY);
  
-//восстановление момента воспроизводства видео при перезагрузке
 player.setCurrentTime(getCurrentTime);
